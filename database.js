067e5c0384491d0be137596f9c2b53905500f08b
Navi026 <karlos123jun@hotmail.com>
2021-04-10 10:47:06 -0500

    back terminado 2

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/supermercado';
+const URI = 'mongodb+srv://admin:admin@cluster0.bxvkg.mongodb.net/supermercado?retryWrites=true&w=majority';
 mongoose.connect(URI,{ useNewUrlParser: true })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
